74b2a41a37ed9629d66a7994c2d1eaaf0b6b139e
Renan <renansantos070805@gmail.com>
2026-05-25 21:59:44 -0300

    banco sql escola

---
+++ escola2.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+conexao = sqlite3.connect("escola.db")
+
+cursor = conexao.cursor()
+
+cursor.execute("SELECT AVG(nota) FROM alunos")
+
+media = cursor.fetchone()[0]
+
+conexao.close()
+
+print("Media das notas:", media)
